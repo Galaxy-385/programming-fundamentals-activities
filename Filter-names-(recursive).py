e9ccc2190091e6f50d283d212c29ced586d921d3
Víctor <vjga2006@gmail.com>
2024-11-14 09:34:16 +0000

    Create Filter-names-(recursive).py

---
+++ Filter-names-(recursive).py
@@ -0,0 +1,17 @@
+#main.py
+import string_utils
+
+names = ['John', 'Paul', 'Martin', 'Peter', 'Max', 'Mia']
+initial = 'M'
+filtered_names = string_utils.filter_names(names, initial)
+print(filtered_names)
+
+
+#string_utils
+def filter_names(names, initial):
+    if len(names) == 0:
+        return []
+    elif names[0][0] != initial:
+        return filter_names(names[1:], initial)
+    elif names[0][0] == initial:
+        return [names[0]] + filter_names(names[1:], initial)
